51c2fa65e109a99bec53bdd09327567c14bcf69e
Zalmat <zalmat@yandex.ru>
2026-06-28 06:01:01 +0500

    Задание №17: Подготовить новый проект для нового учебного приложения

---
+++ mantis_test/src/test/java/ru/stqa/mantis/test/TestBase.java
@@ -0,0 +1,25 @@
+package ru.stqa.mantis.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import ru.stqa.mantis.manager.ApplicationManager;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class TestBase {
+
+    protected static ApplicationManager app;
+
+    @BeforeEach
+    public void setUp() throws IOException {
+        Properties properties = null;
+        if (app == null) {
+            properties = new Properties();
+            properties.load(new FileReader(System.getProperty("target", "local.properties")));
+            app = new ApplicationManager();
+        }
+        app.init(System.getProperty("browser", "firefox"), properties);
+    }
+}
